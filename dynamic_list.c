8807702b714eb35d8580188feb6b2365dca366c8
Your Name <you@example.com>
2017-11-16 13:51:42 -0600

    16/11/2017 13:51

--- dynamic_list.c
+++ dynamic_list.c
@@ -6,10 +6,70 @@ struct list_node
 {
    int value; 
    struct list_node * next; 
-}
+}; 
 
 
 typedef struct list_node list_node; 
+
+
+struct list
+{
+   list_node * head; 
+   int size; 
+}; 
+
+typedef struct list list; 
+
+
+list * create_list()
+{
+   list * l = (list *) malloc(sizeof(list)); 
+   l->head = NULL; 
+   l->size = 0; 
+
+   return l;
+}
+
+
+bool empty_list(list * l) 
+{
+
+   return l->size == 0; 
+
+}
+
+
+void push_front_list(list *l, int value)
+{
+   list_node * ln = (list_node *) malloc(sizeof(list_node)); 
+   ln->value = value; 
+   ln->next = l->head; 
+   l->size++; 
+}
+
+
+int pop_front_list(list * l)
+{
+   if (empty_list(l))
+   {
+      printf("Error"); 
+      return 0; 
+   }
+
+   int value = l->head->value;    
+   list_node * ln = l->head;
+   l->head = l->head->next; 
+   free(ln); 
+   l->size--;
+
+   return value; 
+   
+}
+
+
+
+
+
 
 
 
